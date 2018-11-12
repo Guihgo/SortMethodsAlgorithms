bc36a9ce795fbd367bb56037df9f9813498cb26e
Guihgo <guihgo100milha@gmail.com>
2018-11-12 02:25:14 -0200

    primeira parte da interface

---
+++ AtomicReact/js/Array.js
@@ -0,0 +1,19 @@
+module.exports.onRender = function(atom){
+  var inputNumber = Atomic.getSub(atom, 'inputNumber');
+  var btnAdd = Atomic.getSub(atom, 'btnAdd');
+
+  atom.array = Atomic.getAtom('Array').array;
+
+  btnAdd.onclick = function() {
+    if(inputNumber.value=="") {return alert('Você deve digitar um número para adiciona-lô')}
+    Atomic.add(atom, 'Number', [{key:'number', value: inputNumber.value}]);
+    inputNumber.value = "";
+  };
+}
+
+module.exports.onAdded = function(atomAdded, atom){
+  let btnRemove = Atomic.getSub(atomAdded, 'btnRemove');
+  btnRemove.onclick = function() {
+    Atomic.getNucleus(atom).removeChild(atomAdded);
+  }
+}
--- main_teste.c
+++ main_teste.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "SortMethods.h"
 
+int chooseMethod(){
+
+}
+
 int main(){
     int A[10] = {31, 81, 84, 23, 63, 57, 16, 25, 63, 40};
     quickSort(A, 0, 10);
     printaVetor(A, 10);
+
 }
