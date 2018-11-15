296dd760e8cd3726531d71493d0c4e6dd255c777
jmtzt <jmttozato@gmail.com>
2018-11-15 18:36:50 -0200

    Implementacao partition e quicksort, conferir alguns n_comparacoes e n_trocas

--- AtomicReact/js/SortMethodComparison.js
+++ AtomicReact/js/SortMethodComparison.js
@@ -1,132 +1,132 @@
 module.exports.onRender = function(atom){
-  atom.atomicKey = 'SortMethodComparison';
-  let thisAtom = Atomic.getAtom(atom.atomicKey);
-  atom.getArray = thisAtom.getArray;
-  atom.getMethod = thisAtom.getMethod;
-  atom.doMethod = thisAtom.doMethod;
-  atom.addResult = thisAtom.addResult;
+    atom.atomicKey = 'SortMethodComparison';
+    let thisAtom = Atomic.getAtom(atom.atomicKey);
+    atom.getArray = thisAtom.getArray;
+    atom.getMethod = thisAtom.getMethod;
+    atom.doMethod = thisAtom.doMethod;
+    atom.addResult = thisAtom.addResult;
 
-  let menu = Atomic.getSub(atom, 'menu');
-  let btnComparar = Atomic.getSub(menu, 'btnComparar');
-  let btnLimpar = Atomic.getSub(menu, 'btnLimpar');
+    let menu = Atomic.getSub(atom, 'menu');
+    let btnComparar = Atomic.getSub(menu, 'btnComparar');
+    let btnLimpar = Atomic.getSub(menu, 'btnLimpar');
 
-  btnComparar.onclick = function(){
-    atom.addResult(atom.getMethod());
-    // atom.addResult(atom.getMethod('methodTwo'));
-  }
+    btnComparar.onclick = function(){
+        atom.addResult(atom.getMethod());
+        // atom.addResult(atom.getMethod('methodTwo'));
+    };
 
-  btnLimpar.onclick = function(){
-    let result = Atomic.getSub(atom, 'result');
-    Atomic.getNucleus(Atomic.getSub(result, 'table')).innerHTML = '';
-    Atomic.getNucleus(result).innerHTML = '';
-  }
-}
+    btnLimpar.onclick = function(){
+        let result = Atomic.getSub(atom, 'result');
+        Atomic.getNucleus(Atomic.getSub(result, 'table')).innerHTML = '';
+        Atomic.getNucleus(result).innerHTML = '';
+    };
+    };
 
 module.exports.getArray = function(){
-  let array = Atomic.getNucleus(Atomic.getSub(Atomic.getSub(this, 'menu'), 'array'));
-  let arrayNumber = [];
-  for(let i=0; i<array.children.length; i++) {
-    let number = parseInt(Atomic.getSub(array.children.item(i), 'number').innerHTML);
-    arrayNumber.push(number);
-  }
-  return arrayNumber;
-}
+    let array = Atomic.getNucleus(Atomic.getSub(Atomic.getSub(this, 'menu'), 'array'));
+    let arrayNumber = [];
+    for(let i=0; i<array.children.length; i++) {
+        let number = parseInt(Atomic.getSub(array.children.item(i), 'number').innerHTML);
+        arrayNumber.push(number);
+    }
+    return arrayNumber;
+    };
 
 module.exports.getMethod = function(){
-  let btnMethod = Atomic.getSub(Atomic.getSub(Atomic.getSub(this, 'menu'), 'method'), 'btnMethod');
-  return (btnMethod.innerHTML);
-}
+    let btnMethod = Atomic.getSub(Atomic.getSub(Atomic.getSub(this, 'menu'), 'method'), 'btnMethod');
+    return (btnMethod.innerHTML);
+    };
 
 module.exports.doMethod = function(methodName){
 
-  let methodReturn = {};
-  let array = this.getArray();
-  let Atom = Atomic.getAtom(this.atomicKey);
+    let methodReturn = {};
+    let array = this.getArray();
+    let Atom = Atomic.getAtom(this.atomicKey);
 
-  let timeInitial = performance.now();
+    let timeInitial = performance.now();
 
-  if(methodName=='Bubble') {
-    methodReturn = Atom.bubble(array, array.length);
-    methodReturn.graphicName = 'BubbleSortComplexity.pdf';
-  }
+    if(methodName=='Bubble') {
+        methodReturn = Atom.bubble(array, array.length);
+        methodReturn.graphicName = 'BubbleSortComplexity.pdf';
+    }
 
-  if(methodName=='CocktailShaker') {
-    methodReturn = Atom.cocktailShaker(array, array.length);
-    methodReturn.graphicName = 'CocktailShakerSortComplexity.pdf';
-  }
+    if(methodName=='CocktailShaker') {
+        methodReturn = Atom.cocktailShaker(array, array.length);
+        methodReturn.graphicName = 'CocktailShakerSortComplexity.pdf';
+    }
 
-  if(methodName=='Shell') {
-    methodReturn = Atom.shell(array, array.length);
-    methodReturn.graphicName = 'ShellSortComplexity.pdf';
-  }
+    if(methodName=='Shell') {
+        methodReturn = Atom.shell(array, array.length);
+        methodReturn.graphicName = 'ShellSortComplexity.pdf';
+    }
 
-  if(methodName=='Selection') {
-    methodReturn = Atom.selection(array, array.length);
-    methodReturn.graphicName = 'SelectionSortComplexity.pdf';
-  }
+    if(methodName=='Selection') {
+        methodReturn = Atom.selection(array, array.length);
+        methodReturn.graphicName = 'SelectionSortComplexity.pdf';
+    }
 
-  if(methodName=='Insertion Asc') {
-    methodReturn = Atom.insertionAsc(array, array.length);
-    methodReturn.graphicName = 'InsertionSortComplexity.pdf';
-  }
+    if(methodName=='Insertion Asc') {
+        methodReturn = Atom.insertionAsc(array, array.length);
+        methodReturn.graphicName = 'InsertionSortComplexity.pdf';
+    }
 
-  if(methodName=='Insertion Desc') {
-    methodReturn = Atom.insertionDesc(array, array.length);
-    methodReturn.graphicName = 'InsertionSortComplexity.pdf';
-  }
+    if(methodName=='Insertion Desc') {
+        methodReturn = Atom.insertionDesc(array, array.length);
+        methodReturn.graphicName = 'InsertionSortComplexity.pdf';
+    }
 
-  if(methodName=='Merge') {
-    methodReturn = Atom.mergeSort(array, 0, array.length, Atomic.getAtom(this.atomicKey));
-    methodReturn.graphicName = 'MergeSortComplexity.pdf';
-  }
+    if(methodName=='Merge') {
+        methodReturn = Atom.mergeSort(array, 0, array.length, Atomic.getAtom(this.atomicKey));
+        methodReturn.graphicName = 'MergeSortComplexity.pdf';
+    }
 
-  let timeEnd = performance.now();
+    let timeEnd = performance.now();
 
-  methodReturn.time = (timeEnd-timeInitial);
-  methodReturn.arrayLength = array.length;
-  return methodReturn;
-}
+    methodReturn.time = (timeEnd-timeInitial);
+    methodReturn.arrayLength = array.length;
+    return methodReturn;
+};
 
 module.exports.addResult = function(methodName) {
-  let resultMethod = this.doMethod(methodName);
-  console.log(resultMethod);
+    let resultMethod = this.doMethod(methodName);
+    console.log(resultMethod);
 
-  let result = Atomic.getSub(this, 'result');
-  let resultTable = Atomic.getSub(result, 'table');
+    let result = Atomic.getSub(this, 'result');
+    let resultTable = Atomic.getSub(result, 'table');
 
-  Atomic.add(resultTable, 'ResItem', [
-    {key: 'method', value: methodName},
-    {key: 'arrayLength', value: resultMethod.arrayLength},
-    {key: 'comparisons', value: resultMethod.comparisons},
-    {key: 'swaps', value: resultMethod.swaps},
-    {key: 'time', value: resultMethod.time+"ms"}
-  ]);
+    Atomic.add(resultTable, 'ResItem', [
+        {key: 'method', value: methodName},
+        {key: 'arrayLength', value: resultMethod.arrayLength},
+        {key: 'comparisons', value: resultMethod.comparisons},
+        {key: 'swaps', value: resultMethod.swaps},
+        {key: 'time', value: resultMethod.time+"ms"}
+    ]);
 
-  Atomic.add(result, 'Graphic', [
-    {key: 'title', value: methodName},
-    {key: 'url', value: ('./plots/'+resultMethod.graphicName)}
-  ]);
+    Atomic.add(result, 'Graphic', [
+        {key: 'title', value: methodName},
+        {key: 'url', value: ('./plots/'+resultMethod.graphicName)}
+    ]);
 
-}
+};
 
 /* Sort Methods Implementation */
 
 module.exports.bubble = function(A, n){
     let aux,i,j;
-    let cont = 0, trocas = 0;
+    let n_comparacoes = 0, n_trocas = 0;
     for(i = 1; i < n; i++){
         for(j = 0; j < n - 1; j++){
-            cont++;
+            n_comparacoes = n_comparacoes + 1;
             if(A[j] > A[j+1]){
                 aux = A[j];
                 A[j] = A[j+1];
                 A[j+1] = aux;
-                trocas++;
+                n_trocas = n_trocas + 1;
             }
         }
     }
-    return {comparisons: cont, swaps: trocas};
-}
+    return {comparisons: n_comparacoes, swaps: n_trocas};
+};
 
 module.exports.cocktailShaker = function(arr, n){
     let swapped = true;
@@ -168,10 +168,10 @@ module.exports.cocktailShaker = function(arr, n){
         i++;
     }
     return {comparisons: n_comparacoes, swaps: n_trocas};
-}
+};
 
 module.exports.shell = function(arr, n){
-    // TODO: NUMERO DE TROCAS E COMPARACOES
+    // TODO: CONFERIR TROCAS E COMPARACOES
 
     let h = n/2;
     let key;
@@ -186,17 +186,19 @@ module.exports.shell = function(arr, n){
         for(i = h + 1; i < n; i++){
             key = arr[i];
             j = i;
+            n_comparacoes = n_comparacoes + 1;
             while(arr[j - h] > key){
                 arr[j] = arr[j - h];
                 j = j - h;
                 if(j < h) break;
+                n_trocas = n_trocas + 1;
             }
             arr[j] = key;
         }
     }while(h != 1);
 
-    return {comparisons: -1, swaps: -1};
-}
+    return {comparisons: n_comparacoes, swaps: n_trocas};
+};
 
 module.exports.selection = function(arr, n){
     let menor = 0, temp = 0, n_trocas = 0, n_comparacoes = 0;
@@ -216,10 +218,10 @@ module.exports.selection = function(arr, n){
         }
     }
     return {comparisons: n_comparacoes, swaps: n_trocas};
-}
+};
 
 module.exports.insertionAsc = function(arr, n){
-    let key , count = 0, j;
+    let key , j;
     let n_trocas = 0, n_comparacoes = 0;
     for(let i = 1; i < n; i++){
         key = arr[i];
@@ -233,10 +235,10 @@ module.exports.insertionAsc = function(arr, n){
         arr[j + 1] = key;
     }
     return {comparisons: n_comparacoes, swaps: n_trocas};
-}
+};
 
 module.exports.insertionDesc = function(arr, n){
-    let key , count = 0, j;
+    let key , j;
     let n_trocas = 0, n_comparacoes = 0;
     for(let i = 1; i < n; i++){
         key = arr[i];
@@ -250,7 +252,7 @@ module.exports.insertionDesc = function(arr, n){
         arr[j + 1] = key;
     }
     return {comparisons: n_comparacoes, swaps: n_trocas};
-}
+};
 
 module.exports.merge = function(arr, i1, j1, i2, j2){
     // TODO: NUMERO DE TROCAS E COMPARACOES
@@ -279,7 +281,7 @@ module.exports.merge = function(arr, i1, j1, i2, j2){
     for(i=i1,j=0;i<=j2;i++,j++){
         arr[i]=temp[j];
     }
-}
+};
 
 module.exports.mergeSort = function(arr, i, j, Atom){
     // TODO: NUMERO DE TROCAS E COMPARACOES
@@ -294,6 +296,41 @@ module.exports.mergeSort = function(arr, i, j, Atom){
     }
 
     return {comparisons: -1, swaps: -1};
-}
+};
 
 // TODO: QUICKSORT (E O SEU PARTITION)
+module.exports.partition = function(arr, menor, maior){   
+    // TODO: NUMERO DE TROCAS E COMPARACOES
+    let n_comparacoes = 0;
+    let n_trocas = 0;
+    let pivot = arr[maior];    // pivo
+    let i = (menor - 1);  // menor elemento
+    let temp, temp2;
+    for (let j = menor; j <= maior - 1; j++) 
+    { 
+        // Se o elemento atual e menor igual ao pivot 
+        n_comparacoes = n_comparacoes + 1;
+        if (arr[j] <= pivot) 
+        { 
+            i++;    //aumenta index do menor elemento
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            n_trocas = n_trocas + 1;
+        } 
+    } 
+    temp2 = arr[i+1];
+    arr[i+1] = A[maior];
+    A[maior] = temp2;
+    n_trocas = n_trocas + 1;
+    return (i + 1); 
+};
+
+module.exports.quicksort = function(arr, menor, maior){
+    if(menor < maior){
+        let q = Atom.partition(arr, menor, maior);
+        Atom.quicksort(arr, menor, q - 1);
+        Atom.quicksort(arr, q + 1, maior);
+    }
+
+};
